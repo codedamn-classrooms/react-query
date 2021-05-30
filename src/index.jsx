5607b939240a4204101fb00178036c1c6d2b08bb
Mehul Mohan <mehulmpt@gmail.com>
2021-05-31 03:41:17 +0530

    lab

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const client = new QueryClient()
 
 ReactDOM.render(
-	<React.StrictMode>
+	<QueryClientProvider client={client}>
 		<App />
-	</React.StrictMode>,
+		<ReactQueryDevtools />
+	</QueryClientProvider>,
 	document.getElementById('root')
 )
 
