5250c17ef439ffdcb80a9814478f5bc39f9ecea5
Mehul Mohan <mehulmpt@gmail.com>
2021-05-31 03:57:45 +0530

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { useQuery } from 'react-query'
 
 function App() {
 	// let's go
+	const [postID, setPostID] = useState(1)
+
+	const { data, isLoading } = useQuery(
+		['post', postID],
+		() => fetcher(`https://jsonplaceholder.typicode.com/posts/${postID}`),
+		{
+			staleTime: 0,
+			cacheTime: 300 * 1000, // 5 minutes
+		}
+	)
+
+	if (isLoading) {
+		return <p>Loading post...</p>
+	}
+
+	function loadPost(postID) {
+		setPostID(postID)
+	}
 
 	return (
 		<div className="App">
-			<h2>Blog posts</h2>
+			<button onClick={() => loadPost(1)}>Load Post 1</button>
+			<button onClick={() => loadPost(2)}>Load Post 2</button>
+			<button onClick={() => loadPost(3)}>Load Post 3</button>
+			<h2>{data.title}</h2>
+			<p>{data.body}</p>
 		</div>
 	)
 }
