61b1a4c6915282861dc16dd6fb1b90d907f79e37
Mehul Mohan <mehulmpt@gmail.com>
2023-06-18 15:26:17 +0530

    chore: update to vite

--- src/index.jsx
+++ src/index.jsx
@@ -14,9 +14,3 @@ ReactDOM.render(
 	</QueryClientProvider>,
 	document.getElementById('root')
 )
-
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/#hot-module-replacement
-if (import.meta.hot) {
-	import.meta.hot.accept()
-}
