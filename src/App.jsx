b36dd9101d3518d3bda42a876c3f5693fd02af24
Pranav <pranav.techiegeek@gmail.com>
2024-07-22 11:15:36 +0530

    fix: make the lab workable again

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import React, { useState } from 'react'
-import './App.css'
-import { useQuery } from 'react-query'
+import React, { useState } from "react";
+import "./App.css";
+import { useQuery } from "react-query";
 
 function App() {
-	// let's go
-	const [postID, setPostID] = useState(1)
+	const [postID, setPostID] = useState(1);
 
 	const { data, isLoading } = useQuery(
-		['post', postID],
-		() => fetcher(`https://jsonplaceholder.typicode.com/posts/${postID}`),
+		["post", postID],
+		async () => {
+			const response = await fetch(
+				`https://jsonplaceholder.typicode.com/posts/${postID}`
+			);
+			return response.json();
+		},
 		{
 			staleTime: 0,
 			cacheTime: 300 * 1000, // 5 minutes
 		}
-	)
+	);
 
 	if (isLoading) {
-		return <p>Loading post...</p>
+		return <p>Loading post...</p>;
 	}
 
 	function loadPost(postID) {
-		setPostID(postID)
+		setPostID(postID);
 	}
 
 	return (
@@ -28,10 +32,10 @@ function App() {
 			<button onClick={() => loadPost(1)}>Load Post 1</button>
 			<button onClick={() => loadPost(2)}>Load Post 2</button>
 			<button onClick={() => loadPost(3)}>Load Post 3</button>
-			<h2>{data.title}</h2>
-			<p>{data.body}</p>
+			<h2>{data?.title}</h2>
+			<p>{data?.body}</p>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
-import { QueryClientProvider, QueryClient } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-const client = new QueryClient()
+const client = new QueryClient();
 
-ReactDOM.render(
+const element = document.getElementById("root");
+const root = ReactDOM.createRoot(element);
+
+root.render(
 	<QueryClientProvider client={client}>
 		<App />
 		<ReactQueryDevtools />
-	</QueryClientProvider>,
-	document.getElementById('root')
-)
+	</QueryClientProvider>
+);
